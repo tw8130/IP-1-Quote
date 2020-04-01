13a99c86b48177d8e9f8d86234a13bb8447b1660
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-01 15:42:45 +0300

    added delete quote function in child component

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import {Quote } from '../quote'
 
 @Component({
@@ -10,6 +10,10 @@ export class QuoteDetailsComponent implements OnInit {
   @Input()quote:Quote;
   upvotes=0;
   downvotes=0;
+  @Output()isComplete=new EventEmitter<boolean>();
+  quoteDelete(complete:boolean) {
+    this.isComplete.emit(complete);
+  }
 
   constructor() { }
 
