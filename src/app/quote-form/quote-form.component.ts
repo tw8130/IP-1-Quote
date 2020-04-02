29ebf2c3d7cf4253575281e3209111d0cc3d73eb
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-02 11:31:06 +0300

    add submitQuote function and emitQuote eventemmitter to emit event once submit button is clicked

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  newWords=new Quote(0,"","","");
+
+
+  @Output()emitQuote=new EventEmitter<Quote>();
+  submitQuote() {
+    this.emitQuote.emit(this.newWords);
+  }
 
   constructor() { }
 
